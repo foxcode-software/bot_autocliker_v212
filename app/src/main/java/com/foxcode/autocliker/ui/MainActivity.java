2ff97d3ff98471f98c3e1044e44d25b40b411b3e
foxcode-software <kontakt.intersoft@gmail.com>
2025-12-27 15:04:13 +0100

    Test_2

---
+++ app/src/main/java/com/foxcode/autocliker/ui/MainActivity.java
@@ -0,0 +1,13 @@
+package com.foxcode.autocliker.ui;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.foxcode.autocliker.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // przygotuj prosty layout w res/layout/activity_main.xml
+    }
+}
